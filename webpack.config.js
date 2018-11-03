76c16eb99f10f19d84d86fe0fdd1861b2d3a9627
tuhnik <huntlarsen@gmail.com>
2018-11-03 19:07:43 +0200

    cleaning up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
